f5de5c5539dd6686f0ad3d9b0e308312279b268c
Chiriq <chiriqrp@gmail.com>
2022-06-19 23:26:51 +0300

    change8

--- SQL-EX.ru.sql
+++ SQL-EX.ru.sql
@@ -41,7 +41,18 @@ Where product.model = laptop.model AND laptop.hd >=10
 ===================================
 
 7)Найдите номера моделей и цены всех имеющихся в продаже продуктов (любого типа) производителя B (латинская буква).
-
+SELECT a.model, price 
+FROM (SELECT model, price 
+FROM PC 
+UNION
+SELECT model, price 
+FROM Laptop
+UNION
+SELECT model, price 
+FROM Printer)
+ AS a JOIN 
+ Product p ON a.model = p.model
+WHERE p.maker = 'B'
 
 ===================================
 
